f332b801a67c50ac6709e93a0db5d3964d00148c
hanjingjing <jingjing.han@justalk.com>
2020-04-05 09:13:35 +0800

    按键改为系统播放方法

--- ios/Classes/DTMF.swift
+++ ios/Classes/DTMF.swift
@@ -7,6 +7,23 @@ public typealias DTMFType = (Float, Float)
 public typealias MarkSpaceType = (Float, Float)
 public class DTMF
 {
+
+
+    public static let toneSound1 = UInt32(1200)
+    public static let toneSound2 = UInt32(1201)
+    public static let toneSound3 = UInt32(1202)
+    public static let toneSound4 = UInt32(1203)
+    public static let toneSound5 = UInt32(1204)
+    public static let toneSound6 = UInt32(1205)
+    public static let toneSound7 = UInt32(1206)
+    public static let toneSound8 = UInt32(1207)
+    public static let toneSound9 = UInt32(1208)
+    public static let toneSound0 = UInt32(1209)
+    public static let toneSoundStar = UInt32(1210)
+    public static let toneSoundPound = UInt32(1211)
+    public static let toneSoundCallWaiting = UInt32(1256)
+
+
     public static let tone1     = DTMFType(800.0, 800.0)
     public static let tone2     = DTMFType(1336.0, 697.0)
     public static let tone3     = DTMFType(1477.0, 697.0)
@@ -26,7 +43,7 @@ public class DTMF
     public static let toneCallWaiting     = DTMFType(440.0, 480.0)
     public static let playCallAlert     = DTMFType(350.0, 440.0)
     public static let playCallTerm     = DTMFType(425.0, 425.0)
-    
+
     public static let standard  = MarkSpaceType(40.0, 40.0)
     public static let motorola  = MarkSpaceType(250.0, 250.0)
     public static let whelen    = MarkSpaceType(40.0, 20.0)
@@ -34,12 +51,12 @@ public class DTMF
     public static let long  = MarkSpaceType(1000.0, 40.0)
     public static let middle  = MarkSpaceType(200.0, 40.0)
     public static let short  = MarkSpaceType(100.0, 40.0)
-    
-    
-    
+
+
+
     /**
      Generates a series of Float samples representing a DTMF tone with a given mark and space.
-     
+
      - parameter DTMF: takes a DTMFType comprised of two floats that represent the desired tone frequencies in Hz.
      - parameter markSpace: takes a MarkSpaceType comprised of two floats representing the duration of each in milliseconds. The mark represents the length of the tone and space the silence.
      - parameter sampleRate: the number of samples per second (Hz) desired.
@@ -49,20 +66,20 @@ public class DTMF
     {
         let toneLengthInSamples = 10e-4 * markSpace.0 * sampleRate
         let silenceLengthInSamples = 10e-4 * markSpace.1 * sampleRate
-        
+
         var sound = [Float](repeating: 0, count: Int(toneLengthInSamples + silenceLengthInSamples))
         let twoPI:Float = 2.0 * .pi
-        
+
         for i in 0 ..< Int(toneLengthInSamples) {
             // Add first tone at half volume
             let sample1 = 0.5 * sin(Float(i) * twoPI / (sampleRate / DTMF.0));
-            
+
             // Add second tone at half volume
             let sample2 = 0.5 * sin(Float(i) * twoPI / (sampleRate / DTMF.1));
-            
+
             sound[i] = sample1 + sample2
         }
-        
+
         return sound
     }
 }
@@ -90,7 +107,28 @@ extension DTMF
         case playCallAlert = "Y"
         case playCallTerm = "Z"
     }
-    
+    enum stringForToneSound: String {
+          case tone1     = "1"
+          case tone2     = "2"
+          case tone3     = "3"
+          case tone4     = "4"
+          case tone5     = "5"
+          case tone6     = "6"
+          case tone7     = "7"
+          case tone8     = "8"
+          case tone9     = "9"
+          case tone0     = "0"
+          case toneA     = "A"
+          case toneB     = "B"
+          case toneC     = "C"
+          case toneD     = "D"
+          case toneStar  = "*"
+          case tonePound = "#"
+          case toneCallWaiting = "X"
+          case playCallAlert = "Y"
+          case playCallTerm = "Z"
+      }
+
     public static func toneForCharacter(character: Character) -> DTMFType?
     {
         var tone: DTMFType?
@@ -155,10 +193,10 @@ extension DTMF
         default:
             break
         }
-        
+
         return tone
     }
-    
+
     public static func tonesForString(_ string: String) -> [DTMFType]?
     {
         var tones = [DTMFType]()
@@ -167,7 +205,77 @@ extension DTMF
                 tones.append(tone)
             }
         }
-        
+
         return tones.count > 0 ? tones : nil
     }
+
+
+    public static func toneSoundForString(stringForTone: String) -> UInt32?
+      {
+          var tone: UInt32?
+          switch (stringForTone) {
+          case stringForToneSound.tone1.rawValue:
+              tone = DTMF.toneSound1
+              break
+          case stringForToneSound.tone2.rawValue:
+              tone = DTMF.toneSound2
+              break
+          case stringForToneSound.tone3.rawValue:
+              tone = DTMF.toneSound3
+              break
+          case stringForToneSound.tone4.rawValue:
+              tone = DTMF.toneSound4
+              break
+          case stringForToneSound.tone5.rawValue:
+              tone = DTMF.toneSound5
+              break
+          case stringForToneSound.tone6.rawValue:
+              tone = DTMF.toneSound6
+              break
+          case stringForToneSound.tone7.rawValue:
+              tone = DTMF.toneSound7
+              break
+          case stringForToneSound.tone8.rawValue:
+              tone = DTMF.toneSound8
+              break
+          case stringForToneSound.tone9.rawValue:
+              tone = DTMF.toneSound9
+              break
+          case stringForToneSound.tone0.rawValue:
+              tone = DTMF.toneSound0
+              break
+//          case characterForTone.toneA.rawValue:
+//              tone = DTMF.toneA
+//              break
+//          case characterForTone.toneB.rawValue:
+//              tone = DTMF.toneB
+//              break
+//          case characterForTone.toneC.rawValue:
+//              tone = DTMF.toneC
+//              break
+//          case characterForTone.toneD.rawValue:
+//              tone = DTMF.toneD
+//              break
+          case stringForToneSound.toneStar.rawValue:
+              tone = DTMF.toneSoundStar
+              break
+          case stringForToneSound.tonePound.rawValue:
+              tone = DTMF.toneSoundPound
+              break
+          case stringForToneSound.toneCallWaiting.rawValue:
+              tone = DTMF.toneSoundCallWaiting
+              break
+//          case characterForTone.playCallAlert.rawValue:
+//              tone = DTMF.playCallAlert
+//              break
+//          case characterForTone.playCallTerm.rawValue:
+//              tone = DTMF.playCallTerm
+//              break
+          default:
+              break
+          }
+
+          return tone
+      }
+
 }
--- ios/Classes/SwiftFlutterDtmfPlugin.swift
+++ ios/Classes/SwiftFlutterDtmfPlugin.swift
@@ -12,30 +12,50 @@ public class SwiftFlutterDtmfPlugin: NSObject, FlutterPlugin {
         _engine = AVAudioEngine();
         _player = AVAudioPlayerNode()
         _mixer = _engine.mainMixerNode;
-        
-        _engine.attach(_player)
-        
+        print("SwiftFlutterDtmfPlugin init")
         super.init()
     }
-    
+
     public static func register(with registrar: FlutterPluginRegistrar) {
         let channel = FlutterMethodChannel(name: "flutter_dtmf", binaryMessenger: registrar.messenger())
         let instance = SwiftFlutterDtmfPlugin()
         registrar.addMethodCallDelegate(instance, channel: channel)
     }
-    
+
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         let arguments = call.arguments as? NSDictionary
         if call.method == "playTone"
         {
             guard let digits = arguments?["digits"] as? String else {return}
-            let samplingRate =  arguments?["samplingRate"] as? Double ?? 8000.0
-            playTone(digits: digits, samplingRate: samplingRate)
+            AudioServicesPlaySystemSound(DTMF.toneSoundForString(stringForTone: digits) ?? DTMF.toneSound0)
+            //            let samplingRate =  arguments?["samplingRate"] as? Double ?? 8000.0
+            //            playTone(digits: digits, samplingRate: samplingRate)
         }
         else if call.method == "playCallWaiting"
         {
+            print("playCallWaiting")
+            //            var soundID:SystemSoundID = 0
+            //            //获取声音地址
+            //            let path = Bundle.main.path(forResource: "Ring", ofType: "wav")
+            //            print("@@@ playCallWaitingpath %s", path ?? "@default")
+            //
+            //            //地址转换
+            //            let baseURL = NSURL(fileURLWithPath:path!)
+            //            AudioServicesCreateSystemSoundID(baseURL, &soundID)
+            //
+            //            print("@@@ playCallWaitingsoundID %d", soundID)
+            //            //添加音频结束时的回调
+            //            let observer = UnsafeMutableRawPointer(Unmanaged.passUnretained(self).toOpaque())
+            //            AudioServicesAddSystemSoundCompletion(soundID, nil, nil, {
+            //                (soundID, inClientData) -> Void in
+            //                AudioServicesRemoveSystemSoundCompletion(soundID)
+            //                AudioServicesDisposeSystemSoundID(soundID)
+            //            }, observer)
+            //            AudioServicesPlaySystemSound(soundID)
+
             let samplingRate =  arguments?["samplingRate"] as? Double ?? 8000.0
             playTone(digits: "X", samplingRate: samplingRate, markSpace: DTMF.long)
+            result(nil)
         }
         else if call.method == "playCallAlert"
         {
@@ -47,27 +67,26 @@ public class SwiftFlutterDtmfPlugin: NSObject, FlutterPlugin {
             let samplingRate =  arguments?["samplingRate"] as? Double ?? 8000.0
             playTone(digits: "Z", samplingRate: samplingRate, markSpace: DTMF.middle)
         }
-        
+
     }
-    
+
     func playTone(digits: String, samplingRate: Double, markSpace: MarkSpaceType = DTMF.motorola)
     {
-        
         let _sampleRate = Float(samplingRate)
-        
+
         if let tones = DTMF.tonesForString(digits) {
             let audioFormat = AVAudioFormat(commonFormat: .pcmFormatFloat32, sampleRate: Double(_sampleRate), channels: 2, interleaved: false)!
-            
+
             // fill up the buffer with some samples
             var allSamples = [Float]()
             for tone in tones {
                 let samples = DTMF.generateDTMF(tone, markSpace: markSpace, sampleRate: _sampleRate)
                 allSamples.append(contentsOf: samples)
             }
-            
+
             let frameCount = AVAudioFrameCount(allSamples.count)
             let buffer = AVAudioPCMBuffer(pcmFormat: audioFormat, frameCapacity: frameCount)!
-            
+
             buffer.frameLength = frameCount
             let channelMemory = buffer.floatChannelData!
             for channelIndex in 0 ..< Int(audioFormat.channelCount) {
@@ -75,22 +94,43 @@ public class SwiftFlutterDtmfPlugin: NSObject, FlutterPlugin {
                 memcpy(frameMemory, allSamples, Int(frameCount) * MemoryLayout<Float>.size)
             }
 
-            if _engine.isRunning{
-                _engine.pause()
-                _engine.detach(_player)
+            if _player.isPlaying{
+                print("playTone isPlaying")
+                _player.stop()
+            }else{
+                print("playTone is not Playing")
+
             }
+            if _engine.isRunning{
+                _engine.detach(_player)
+                _engine.pause()
+            }
+
             _engine.attach(_player)
             _engine.connect(_player, to:_mixer, format:audioFormat)
-            
+            _engine.prepare()
+
             do {
                 try _engine.start()
+                _player.scheduleBuffer(buffer, at:nil,completionHandler:nil)
+
+                //挂断时被终止了
+                if _engine.isRunning {
+                    print("playTone isRunning")
+
+                    _player.play()
+
+                    print("playTone Running")
+
+                }else{
+                    print("playTone is not Running")
+
+                }
             } catch let error as NSError {
                 print("Engine start failed - \(error)")
+
             }
-            
-            _player.scheduleBuffer(buffer, at:nil,completionHandler:nil)
-            _player.play()
-            
+
         }
     }
 }
