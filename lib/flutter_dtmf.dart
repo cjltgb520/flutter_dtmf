c17100d9577be9c2447a5928c3c5a0bbb6f48d51
陈金龙 <jinlong.chen@justalk.com>
2024-08-26 15:01:20 +0800

    change: 更新依赖

--- lib/flutter_dtmf.dart
+++ lib/flutter_dtmf.dart
@@ -1,11 +1,9 @@
 import 'dart:async';
 
-import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class FlutterDtmf {
-  static const MethodChannel _channel =
-  const MethodChannel('flutter_dtmf');
+  static const MethodChannel _channel = const MethodChannel('flutter_dtmf');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
@@ -13,26 +11,31 @@ class FlutterDtmf {
   }
 
   static Future<void> playTone(
-      {@required String digits, double samplingRate}) async {
+      {required String? digits, double? samplingRate}) async {
     assert(digits != null);
-    final Map<String, Object> args = <String, dynamic>{
+    final Map<String, dynamic> args = <String, dynamic>{
       'digits': digits,
       'samplingRate': samplingRate
     };
     await _channel.invokeMethod('playTone', args);
   }
+
   static Future<void> playCallWaiting() async {
     await _channel.invokeMethod('playCallWaiting');
   }
+
   static Future<void> playCallAlert() async {
     await _channel.invokeMethod('playCallAlert');
   }
+
   static Future<void> playCallTerm() async {
     await _channel.invokeMethod('playCallTerm');
   }
+
   static Future<void> releaseGenerator() async {
     await _channel.invokeMethod('releaseGenerator');
   }
+
   static Future<void> pauseCallWaiting() async {
     await _channel.invokeMethod('pauseCallWaiting');
   }
